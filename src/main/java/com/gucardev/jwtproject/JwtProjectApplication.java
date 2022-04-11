d041e0ccf3b78d2f993df9c6457c8ee87c457e85
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-11 16:09:45 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/jwtproject/JwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.jwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/jwtproject/JwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.jwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
