51bb1c2e9eca741f636f3cf61d7289168d322947
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-12 01:19:28 +0300

    new query added to refresh token repository

--- src/main/java/com/gucardev/jwtproject/repository/RefreshTokenRepository.java
+++ src/main/java/com/gucardev/jwtproject/repository/RefreshTokenRepository.java
@@ -4,6 +4,12 @@ import com.gucardev.jwtproject.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+
 }
