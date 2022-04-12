9678e90cb205eeab9085af8bc343e1a05d2511de
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-12 12:00:12 +0300

    test controller created

---
+++ src/main/java/com/gucardev/jwtproject/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.gucardev.jwtproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.ok("Hello!");
+    }
+
+
+}
