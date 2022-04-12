f32194642083adbdf08a78c612cf2a0a9f7fd8d1
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-12 16:17:21 +0300

    fake data added

--- src/main/java/com/gucardev/jwtproject/JwtProjectApplication.java
+++ src/main/java/com/gucardev/jwtproject/JwtProjectApplication.java
@@ -1,5 +1,6 @@
 package com.gucardev.jwtproject;
 
+import com.gucardev.jwtproject.model.ROLES;
 import com.gucardev.jwtproject.model.Role;
 import com.gucardev.jwtproject.model.User;
 import com.gucardev.jwtproject.service.RoleService;
@@ -29,14 +30,22 @@ public class JwtProjectApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         roleService.saveRole(Role.builder().name("USER").detail("simple operations").build());
-        roleService.saveRole(Role.builder().name("ADMIN").detail("simple operations").build());
-        User user = userService.saveUser(User.builder().username("gurkan").email("g@mail.com").password("password").build());
+        roleService.saveRole(Role.builder().name("ADMIN").detail("admin operations").build());
+        roleService.saveRole(Role.builder().name("SUPERADMIN").detail("super operations").build());
+        User superadmin = userService.saveUser(User.builder().username("gurkan").email("g@mail.com").password("password").build());
         userService.saveUser(User.builder().username("metin").email("m@mail.com").password("password").build());
         userService.saveUser(User.builder().username("ali").email("a@mail.com").password("password").build());
+        userService.saveUser(User.builder().username("kartal").email("k@mail.com").password("password").build());
+        User admin = userService.saveUser(User.builder().username("sezai").email("s@mail.com").password("password").build());
 
-        var adminRole = roleService.getRoleByName("ADMIN");
-        user.setRoles(List.of(adminRole));
-        userService.updateUser(user);
+
+        var superAdminRole = roleService.getRoleByName(ROLES.SUPERADMIN.toString());
+        superadmin.setRoles(List.of(superAdminRole));
+        userService.updateUser(superadmin);
+
+        var adminRole = roleService.getRoleByName(ROLES.ADMIN.toString());
+        admin.setRoles(List.of(adminRole));
+        userService.updateUser(admin);
 
 
     }
