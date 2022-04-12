e9e67e18fcf357fee6edff2cdfb0cbea3bf38f2e
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-04-12 16:42:36 +0300

    UpdateUserRequest created

---
+++ src/main/java/com/gucardev/jwtproject/request/UpdateUserRequest.java
@@ -0,0 +1,41 @@
+package com.gucardev.jwtproject.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class UpdateUserRequest {
+
+    @NotNull
+    @Size(max = 50, min = 3)
+    private String username;
+
+    @NotNull
+    @Email
+    private String email;
+
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @NotBlank
+    private String surname;
+
+    @NotNull
+    @Size(max = 50, min = 8)
+    private String password;
+
+
+}
